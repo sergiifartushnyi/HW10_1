d08ef47448ced876ab565b27323c6989767a04cd
sergiifartushnyi <sergiyfartyshnyy@gmail.com>
2024-08-27 19:50:18 +0300

    HW 10_1

---
+++ 10_1.py
@@ -0,0 +1,20 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+    begin: перший елемент послідовності
+    end: кількість елементів у послідовності
+    func: функція, яка формує значення для послідовності
+    """
+
+    for _ in range(end):
+        yield begin
+        begin = func(begin)
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+print(isgenerator(gen))
+print(list(gen))
+print('OK')
